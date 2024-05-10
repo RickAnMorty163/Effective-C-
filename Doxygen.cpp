d0a82420803028a1ae3f49ac0f7214c5e9db10d7
LiXiang <rickanmorty@163.com>
2024-05-10 09:46:03 +0800

    "daily update"

--- Doxygen.cpp
+++ Doxygen.cpp
@@ -8,8 +8,8 @@
  * @details the function may throw some exception like accept C format cahr
  * and string object at the same time
  */
-const std::string Strcat(const std::string& s1, const std::string& s2) {
-    return s1 + s2;
+const std::string Strcat(const std::string &s1, const std::string &s2) {
+  return s1 + s2;
 }
 
 /**
@@ -18,22 +18,22 @@ const std::string Strcat(const std::string& s1, const std::string& s2) {
  * @return const string& randomSquence
  */
 const std::string RandomString(std::size_t t = 8) {
-    std::string randomSequence;
-    while (t) {
-        auto randSeed{65 + std::rand() % (122 + 1 - 65)};
-        if (randSeed >= 91 && randSeed <= 96)
-            continue;
-        randomSequence += static_cast<char>(randSeed);
-        t--;
-    }
-    return randomSequence;
+  std::string randomSequence;
+  while (t) {
+    auto randSeed{65 + std::rand() % (122 + 1 - 65)};
+    if (randSeed >= 91 && randSeed <= 96)
+      continue;
+    randomSequence += static_cast<char>(randSeed);
+    t--;
+  }
+  return randomSequence;
 }
 
 signed main() {
-    auto s_one{RandomString(42)};
-    auto s_two{RandomString(10)};
-    auto s_noparam{RandomString()};
-    std::cout << s_one << std::endl;
-    std::cout << s_two << std::endl;
-    std::cout << s_noparam << std::endl;
+  auto s_one{RandomString(42)};
+  auto s_two{RandomString(10)};
+  auto s_noparam{RandomString()};
+  std::cout << s_one << std::endl;
+  std::cout << s_two << std::endl;
+  std::cout << s_noparam << std::endl;
 }
